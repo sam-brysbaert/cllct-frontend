1187c4b4f69ee1c7694593e0d795ef075d676cb7
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-04-29 22:32:03 +0200

    fixed bug where button style and animation wasn't working properly

--- src/app/link/link.module.ts
+++ src/app/link/link.module.ts
@@ -6,14 +6,25 @@ import { MatCardModule } from '@angular/material/card';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { MatTreeModule } from '@angular/material/tree';
 import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     LinkListComponent,
     LinkDetailsComponent,
     CategoryListComponent,
   ],
-  imports: [CommonModule, MatCardModule, MatTreeModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatTreeModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LayoutModule,
+  ],
   exports: [LinkListComponent, LinkDetailsComponent, CategoryListComponent],
 })
 export class LinkModule {}
