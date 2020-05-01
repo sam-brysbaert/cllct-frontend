fdb3c12859b2ab8685b3b3129bec72d206c32c2f
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-05-01 17:25:16 +0200

    add collapse for navigation + animations

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -4,13 +4,39 @@ import { NestedTreeControl } from '@angular/cdk/tree';
 import { Category } from '../category';
 import { CategoryDataService } from '../category-data.service';
 import { Observable } from 'rxjs';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss'],
+  animations: [
+    trigger('smoothCollapse', [
+      state(
+        'show',
+        style({
+          height: '0',
+          overflow: 'hidden',
+        })
+      ),
+      state(
+        '*',
+        style({
+          overflow: 'hidden',
+        })
+      ),
+      transition('* => *', [animate('200ms cubic-bezier(0.25, 0.8, 0.25, 1)')]),
+    ]),
+  ],
 })
 export class CategoryListComponent implements OnInit {
+  public isCollapsed: boolean = true;
   private _fetchCategories$: Observable<Category[]>;
   // tree view
   treeControl = new NestedTreeControl<Category>((node) => node.children);
@@ -20,11 +46,9 @@ export class CategoryListComponent implements OnInit {
 
   ngOnInit(): void {
     this._fetchCategories$ = this.categoryDataService.getCategories();
-    this.categories$.subscribe((response) => {
-      this.dataSource.data = response;
-      this.treeControl.dataNodes = response;
-      this.treeControl.expandAll(); // for some reason expand and collapse have opposite functionality to what you would expect, no idea why
-    });
+    this._fetchCategories$.subscribe(
+      (response) => (this.dataSource.data = response)
+    );
   }
 
   hasChild = (_: number, node: Category) =>
