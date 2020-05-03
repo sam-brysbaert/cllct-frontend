0657c12d527e2a724323693baf315083d1d46e6a
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-05-03 23:21:59 +0200

    add cookie for storing dark theme status

--- src/app/core/theme.service.ts
+++ src/app/core/theme.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +9,21 @@ export class ThemeService {
   private _darkTheme = new Subject<boolean>();
   isDarkTheme = this._darkTheme.asObservable();
 
-  constructor() {}
+  constructor(private cookieService: CookieService) {
+    this.setFromCookie();
+  }
+
+  setFromCookie() {
+    // this needs to happen synchronously, otherwise the cookie will not yet be available to read
+    setTimeout(() => {
+      this._darkTheme.next(
+        this.cookieService.get('darkThemeEnabled') === 'true'
+      );
+    });
+  }
 
   setDarkTheme(isDarkTheme: boolean): void {
     this._darkTheme.next(isDarkTheme);
+    this.cookieService.set('darkThemeEnabled', isDarkTheme ? 'true' : 'false');
   }
 }
