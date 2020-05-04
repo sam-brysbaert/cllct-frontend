e76b6fddb81079f78f4de71d2183c5bd0fdc9c97
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-05-04 10:20:06 +0200

    Revert "add cookie for storing dark theme status"
    This reverts commit 0657c12d527e2a724323693baf315083d1d46e6a.

--- src/app/core/theme.service.ts
+++ src/app/core/theme.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,21 +8,9 @@ export class ThemeService {
   private _darkTheme = new Subject<boolean>();
   isDarkTheme = this._darkTheme.asObservable();
 
-  constructor(private cookieService: CookieService) {
-    this.setFromCookie();
-  }
-
-  setFromCookie() {
-    // this needs to happen synchronously, otherwise the cookie will not yet be available to read
-    setTimeout(() => {
-      this._darkTheme.next(
-        this.cookieService.get('darkThemeEnabled') === 'true'
-      );
-    });
-  }
+  constructor() {}
 
   setDarkTheme(isDarkTheme: boolean): void {
     this._darkTheme.next(isDarkTheme);
-    this.cookieService.set('darkThemeEnabled', isDarkTheme ? 'true' : 'false');
   }
 }
