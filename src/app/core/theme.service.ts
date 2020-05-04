13bf5f3c7a038374597a47206396da9ffe01b56b
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-05-04 10:37:14 +0200

    use localstorage to persist dark theme status

--- src/app/core/theme.service.ts
+++ src/app/core/theme.service.ts
@@ -8,9 +8,16 @@ export class ThemeService {
   private _darkTheme = new Subject<boolean>();
   isDarkTheme = this._darkTheme.asObservable();
 
-  constructor() {}
+  constructor() {
+    // this has to be done synchronously, otherwise
+    // localstorage will not yet be available to read
+    setTimeout(() => {
+      this._darkTheme.next(localStorage.getItem('darkThemeEnabled') === 'true');
+    });
+  }
 
   setDarkTheme(isDarkTheme: boolean): void {
+    localStorage.setItem('darkThemeEnabled', isDarkTheme ? 'true' : 'false');
     this._darkTheme.next(isDarkTheme);
   }
 }
