5bf89f7baeede6e79e592bcf0c18c8fd6dbf74d4
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-05-14 16:18:10 +0200

    change logo visibility logic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { FilterTermService } from '../services/filter-term.service';
 import { AuthenticationService } from '../user/authentication.service';
 import { Router } from '@angular/router';
+import { HandsetService } from '../services/handset.service';
 
 @Component({
   selector: 'app-header',
@@ -13,16 +14,19 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   isDarkTheme: Observable<boolean>;
   user$;
+  isHandset$: Observable<boolean>;
   constructor(
     private themeService: ThemeService,
     private filterTermService: FilterTermService,
     private authenticationService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private handsetService: HandsetService
   ) {}
 
   ngOnInit(): void {
     this.isDarkTheme = this.themeService.isDarkTheme;
     this.user$ = this.authenticationService.user$;
+    this.isHandset$ = this.handsetService.isHandset$;
   }
 
   toggleDarkTheme(checked: boolean) {
